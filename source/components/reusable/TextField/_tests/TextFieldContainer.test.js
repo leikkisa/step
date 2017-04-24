33d50c7930d36baee41f3eab46ff45cf98ee747d
leikkisa <sallymaki@gmail.com>
2017-04-24 15:01:54 -0700

    start work on handleKeyPress test

--- source/components/reusable/TextField/_tests/TextFieldContainer.test.js
+++ source/components/reusable/TextField/_tests/TextFieldContainer.test.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import sinon from 'sinon'
 import { mount } from 'enzyme'
-import { expect } from '../../../../../configuration/testSetup'
+import moxios from 'moxios'
+import makeEditable from '../TextFieldContainer'
+import { expect } from 'sym/configuration/testSetup'
 import TextFieldContainer from '../TextFieldContainer'
+import globalState from 'sym/source/components/utilities/globalState'
 
-describe( '<TextFieldContainer />', () => {
-  let warnStub, wrapper, makeEditableSpy
+describe.only( '<TextFieldContainer />', () => {
+  let warnStub, wrapper, makeEditableSpy, makeKeyPressProjectSpy, makeKeyPressCouldDoSpy
 
   beforeEach( () => {
+    moxios.install()
     warnStub = sinon.stub( console, 'warn' ).callsFake( () => null )
     makeEditableSpy = sinon.spy( TextFieldContainer.prototype, 'makeEditable' )
-    wrapper = mount( <TextFieldContainer /> )
+    makeKeyPressProjectSpy = sinon.spy( globalState, 'updateProjectText')
+    makeKeyPressCouldDoSpy = sinon.spy( globalState, 'updateCouldDoText')
+    wrapper = mount( <TextFieldContainer type='project' /> )
+    // wrapperCouldDo = mount()
   })
 
   afterEach( () => {
     warnStub.restore()
     makeEditableSpy.restore()
+    makeKeyPressProjectSpy.restore()
+    makeKeyPressCouldDoSpy.restore()
     wrapper.unmount()
+    moxios.uninstall()
   })
 
   it( 'renders .text-field-container', () =>
@@ -32,6 +42,29 @@ describe( '<TextFieldContainer />', () => {
     expect( wrapper.find( 'TextField' ).length ).to.equal( 1 )
   )
 
-  // Write test for handleKeyPress
+  it( 'updates global state with new project when type is project', done => {
+    makeEditable()
+    let something = {key:'Enter'}
+    handleKeyPress(something)
+    moxios.wait( () => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: ''
+      }).then( () => {
+        expect( makeKeyPressProjectSpy.calledOnce ).to.equal( true )
+        done()
+      }).catch( done )
+    })
+  })
+
+  // it( 'updates global state with new couldDo when type is couldDo', () =>
+  //   // wrapper.setProps({ type: ''})
+  //   expect( makeKeyPressCouldDoSpy.calledOnce ).to.equal( true )
+  // )
+
+  // it( 'textFieldContainer error handler', () =>
+  //   expect( wrapper.find( 'TextField' ).length ).to.equal( 1 )
+  // )
 
 })
