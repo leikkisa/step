01a47cc7f2a5ccac0cca3200024599b102c7510f
leikkisa <sallymaki@gmail.com>
2017-04-25 13:51:37 -0700

    Finish handleKeyPress Tests

--- source/components/reusable/TextField/_tests/TextFieldContainer.test.js
+++ source/components/reusable/TextField/_tests/TextFieldContainer.test.js
@@ -23,7 +23,6 @@ describe.only('<TextFieldContainer />', () => {
     makeKeyPressCouldDoSpy = sinon.spy(globalState, 'updateCouldDoText')
     wrapper = mount( < TextFieldContainer / > )
     globalState.set(mockGlobalState)
-    // wrapperCouldDo = mount()
   })
 
   afterEach(() => {
@@ -41,84 +40,102 @@ describe.only('<TextFieldContainer />', () => {
 
   it('calls makeEditable on click', () => {
     wrapper.find('.text-field-container').simulate('click')
-    expect(makeEditableSpy.calledOnce).to.equal(true)
+    expect( makeEditableSpy.calledOnce ).to.equal( true )
   })
 
-  it('renders <TextField />', () =>
-    expect(wrapper.find('TextField').length).to.equal(1)
+  it( 'renders <TextField />', () =>
+    expect(wrapper.find('TextField').length).to.equal( 1 )
   )
 
-  it('updates global state with new project when type is project', done => {
-    wrapper = mount( < TextFieldContainer type='project' / > )
-    wrapper.instance().makeEditable()
-    let event = {
-      key: 'Enter'
-    }
-    wrapper.instance().handleKeyPress(event)
-    moxios.wait( () => {
-      const request = moxios.requests.mostRecent()
-      request.respondWith({
-        status: 200,
-        response: ''
-      }).then( () => {
-        expect( makeKeyPressProjectSpy.calledOnce ).to.equal( true )
-        expect( makeKeyPressCouldDoSpy.notCalled ).to.equal( true )
-        done()
-      }).catch(done)
+  context( 'Tests for handleKeyPress', () => {
+
+    it( 'updates global state with new project when type is project', done => {
+      wrapper = mount( < TextFieldContainer type='project' / > )
+      wrapper.instance().makeEditable()
+      let event = {
+        key: 'Enter'
+      }
+      wrapper.instance().handleKeyPress( event )
+      moxios.wait( () => {
+        const request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: ''
+        }).then( () => {
+          expect( makeKeyPressProjectSpy.calledOnce ).to.equal( true )
+          expect( makeKeyPressCouldDoSpy.notCalled ).to.equal( true )
+          done()
+        }).catch( done )
+      })
+    })
+
+    it( 'updates global state with new couldDo when type is couldDo', done => {
+      wrapper = mount( < TextFieldContainer type='could-do' / > )
+      wrapper.instance().makeEditable()
+      let event = {
+        key: 'Enter'
+      }
+      wrapper.instance().handleKeyPress( event )
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: ''
+        }).then(() => {
+          expect( makeKeyPressCouldDoSpy.calledOnce ).to.equal( true )
+          expect( makeKeyPressProjectSpy.notCalled ).to.equal( true )
+          done()
+        }).catch( done )
+      })
+    })
+
+    it( 'keyHandlePress sets state to false after enter', done => {
+      wrapper = mount( < TextFieldContainer type='could-do' projectId='2' id='2' text='Sally Moos' / > )
+      wrapper.instance().makeEditable()
+      let event = {
+        key: 'Enter'
+      }
+      wrapper.instance().handleKeyPress( event )
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: ''
+        }).then(() => {
+          expect( wrapper.state().editing ).to.equal( false )
+          done()
+        }).catch( done )
+      })
+    })
+
+    it( 'handleKeyPress error handler', done => {
+      wrapper = mount( < TextFieldContainer type='could-do' projectId='2' id='2' text='Sally Moos' / > )
+      wrapper.instance().makeEditable()
+      let event = {
+        key: 'Enter'
+      }
+      wrapper.instance().handleKeyPress(event)
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 400,
+          response: ''
+        }).then(() => {
+          expect( warnStub.calledTwice ).to.equal( true )
+          done()
+        }).catch( done )
+      })
     })
   })
 
-  it('updates global state with new couldDo when type is couldDo', done => {
-    wrapper = mount( < TextFieldContainer type='could-do' / > )
-    wrapper.instance().makeEditable()
-    let event = {
-      key: 'Enter'
-    }
-    wrapper.instance().handleKeyPress(event)
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent()
-      request.respondWith({
-        status: 200,
-        response: ''
-      }).then(() => {
-        expect( makeKeyPressCouldDoSpy.calledOnce ).to.equal(true)
-        expect( makeKeyPressProjectSpy.notCalled ).to.equal( true )
-        done()
-      }).catch(done)
-    })
-  })
-
-  it('editInput changes state to input value', () => {
+  it( 'editInput changes state to input value', () => {
     let event = {
       target: {
         value: 'Cow Cow Moo Moo'
       }
     }
     wrapper.instance().editInput(event)
-    expect(wrapper.state().inputValue).to.equal('Cow Cow Moo Moo')
+    expect( wrapper.state().inputValue ).to.equal( 'Cow Cow Moo Moo' )
   })
-
-  it('keyHandlePress sets state to false after enter', done => {
-    wrapper = mount( < TextFieldContainer type='could-do' projectId='2' id='2' text='Sally Moos' / > )
-    wrapper.instance().makeEditable()
-    let event = {
-      key: 'Enter'
-    }
-    wrapper.instance().handleKeyPress(event)
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent()
-      request.respondWith({
-        status: 200,
-        response: ''
-      }).then(() => {
-        expect(wrapper.state().editing).to.equal(false)
-        done()
-      }).catch(done)
-    })
-  })
-
-  it( 'textFieldContainer error handler', () =>
-    expect( wrapper.find( 'TextField' ).length ).to.equal( 1 )
-  )
 
 })
